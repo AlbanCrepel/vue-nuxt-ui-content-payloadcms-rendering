acba1020cb64d4a5a8741dbc88ca069596f4b455
Adrian Rudnik <adrian@klonmaschine.de>
2025-02-24 11:25:37 +0100

    fix: Reduced whole page width to fit proses width

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,7 @@ import payloadApiResult from '../data/example.json'
 </script>
 
 <template>
-  <div class="max-w-2xl mx-auto">
+  <div class="max-w-xl mx-auto">
     <div class="pt-6 bg-white px-4">
       <article class="prose" style="padding-bottom: 800px;">
         <PayloadRichText :content="payloadApiResult.content"/>
