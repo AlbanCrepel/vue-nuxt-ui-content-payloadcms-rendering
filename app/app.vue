7330ff373e99f23bac3520f0601d3b22a6181416
Adrian Rudnik <adrian@klonmaschine.de>
2025-02-24 11:19:17 +0100

    feat: Added some space to the bottom to allow the inspector to not overlay the content

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,7 @@ import payloadApiResult from '../data/example.json'
 <template>
   <div class="max-w-2xl mx-auto">
     <div class="pt-6 bg-white px-4">
-      <article class="prose pb-60">
+      <article class="prose" style="padding-bottom: 800px;">
         <PayloadRichText :content="payloadApiResult.content"/>
       </article>
     </div>
